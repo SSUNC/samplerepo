b955f85372d7dcf8b2b636490b977b22800f721a
Sundar <33153747+SSUNC@users.noreply.github.com>
2025-01-06 09:33:40 +0530

    fix : update the print statement

--- Forecasting.py
+++ Forecasting.py
@@ -2,4 +2,4 @@ import math
 import pandas as pd
 import numpy as np
 
-print("this py i am going to use for forecast generation")
+print("this py i am going to use for forecast generation using ML")
